9baea79cfebeb386c72fea6fc2a19ac18b7bc715
park hyunjin <genie.park@apptest.ai>
2021-02-11 16:04:23 +0900

    sync

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
-python3 train.py --sensitive --imgW 256 --imgH 32 --batch_size 32 --batch_max_length 32 --PAD \
+python3 train.py --sensitive --imgW 256 --imgH 32 --batch_size 48 --batch_max_length 32 --PAD \
+--saved_model ./saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111/best_accuracy.pth --lr 0.5 \
+--num_iter 3000000 --valInterval 30000  \
 --train_data lmdb_card/train --valid_data lmdb_card/val \
 --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn
